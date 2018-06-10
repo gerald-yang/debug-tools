3d879869895bdd33becee9bf849aa9897f4e903b
Gerald Yang <liming.yang@easystack.cn>
2018-06-10 15:04:25 +0800

    Add an option to transfer data to svg directly.

--- genfl.sh
+++ genfl.sh
@@ -15,17 +15,29 @@ if ! rpm -q sysstat 2>&1 > /dev/null; then
 	yum install -y sysstat
 fi
 
-sar -A > "$CDATE".sar
-ps auxS > "$CDATE".ps
-perf record -ag sleep 300
+if [ -z "$1" ]; then
+	sar -A > "$CDATE".sar
+	ps auxS > "$CDATE".ps
+	perf record -ag sleep 300
 
-if [ -f "./perf.data" ]; then
-	perf script > perf.data.script
-	./flamegraph/stackcollapse-perf.pl perf.data.script > out.fold
-	./flamegraph/flamegraph.pl out.fold > "$CDATE".svg
-	mv perf.data perf.data."$CDATE"
-	rm -f perf.data.script out.fold
+	if [ -f "./perf.data" ]; then
+		perf script > perf.data.script
+		./flamegraph/stackcollapse-perf.pl perf.data.script > out.fold
+		./flamegraph/flamegraph.pl out.fold > "$CDATE".svg
+		mv perf.data perf.data."$CDATE"
+		rm -f perf.data.script out.fold
+	else
+		echo "Failed to generate perf.data"
+		exit 1
+	fi
 else
-	echo "Failed to generate perf.data"
-	exit 1
+	if [ -f "$1" ]; then
+		perf script "$1" > "$1".script
+		./flamegraph/stackcollapse-perf.pl "$1".script > "$1".fold
+		./flamegraph/flamegraph.pl "$1".fold > "$1".svg
+		rm -f "$1".script "$1".fold
+	else
+		echo "Failed to open $1"
+		exit 1
+	fi
 fi
