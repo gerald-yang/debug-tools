4b676096dbd94125a193718f3a6848ebf8a2bf7d
Gerald Yang <gerald.yang.tw@gmail.com>
2020-11-03 22:25:12 +0800

    Fix get ddeb

--- get-ddeb-from-ppa/get-ddeb.sh
+++ get-ddeb-from-ppa/get-ddeb.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+
+VER="14.2.11-0ubuntu0.19.10.1~cloud2"
+UCA="train"
+
 grep 'amd64' list | grep 'dbgsym' | awk '{print $1}'> amd64
 
-for i in `dpkg -l | grep '14.2.11-0ubuntu0.19.10.1~cloud2' | awk '{print $2}' | sed ':a;N;$!ba;s/\n/-dbgsym /g'`; do
+for i in `dpkg -l | grep "$VER" | awk '{print $2}' | sed ':a;N;$!ba;s/\n/-dbgsym /g'`; do
 	echo "search $i"
 	PKG=$(grep "$i" amd64)
 	if [ -z "$PKG" ]; then
 		echo "$PKG not found"
 	else
 		echo "get $PKG"
-		#wget https://launchpad.net/~ubuntu-cloud-archive/+archive/ubuntu/train-updates-ddeb/+files/$PKG
+		wget https://launchpad.net/~ubuntu-cloud-archive/+archive/ubuntu/"$UCA"-updates-ddeb/+files/$PKG
 	fi
 done
