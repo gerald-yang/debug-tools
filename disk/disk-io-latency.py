bff2b4b41d465c75e823afd8d2a4e47811eb04bc
Gerald Yang <gerald.yang.tw@gmail.com>
2020-11-25 20:52:47 +0800

    Add arguments

--- disk/disk-io-latency.py
+++ disk/disk-io-latency.py
@@ -17,9 +17,9 @@ from __future__ import print_function
 from bcc import BPF
 import ctypes as ct
 import re
+import argparse
 
-# load BPF program
-b = BPF(text="""
+bpf_text="""
 #include <uapi/linux/ptrace.h>
 #include <linux/blkdev.h>
 
@@ -32,6 +32,7 @@ struct data_t {
     u32 pid;
     u64 rwflag;
     u64 delta;
+    u64 sector;
     u64 len;
     u64 ts;
     char disk_name[DISK_NAME_LEN];
@@ -42,13 +43,25 @@ BPF_HASH(start, struct request *);
 BPF_HASH(infobyreq, struct request *, struct val_t);
 BPF_PERF_OUTPUT(events);
 
+static int strcmp_workaround(char *name, char *osd_name, int len)
+{
+    int i;
+
+    for (i = 0; i < len; i++) {
+        if (name[i] != osd_name[i])
+            return 1;
+    }
+
+    return 0;
+}
+
 // cache PID and comm by-req
 int trace_pid_start(struct pt_regs *ctx, struct request *req)
 {
     struct val_t val = {};
 
     if (bpf_get_current_comm(&val.name, sizeof(val.name)) == 0) {
-        if (val.name[0] == 't' && val.name[1] == 'p' && val.name[2] == '_' && val.name[3] == 'o' && val.name[4] == 's' && val.name[5] == 'd') {
+        if (strcmp_workaround(req->rq_disk->disk_name, "##ROOTDISK##", ##ROOTDISKLEN##)) {
             val.pid = bpf_get_current_pid_tgid();
             infobyreq.update(&req, &val);
         }
@@ -61,8 +74,10 @@ int trace_req_start(struct pt_regs *ctx, struct request *req)
 {
     u64 ts;
 
-    ts = bpf_ktime_get_ns();
-    start.update(&req, &ts);
+    if (infobyreq.lookup(&req)) {
+        ts = bpf_ktime_get_ns();
+        start.update(&req, &ts);
+    }
 
     return 0;
 }
@@ -84,20 +99,22 @@ int trace_req_completion(struct pt_regs *ctx, struct request *req)
     }
     ts = bpf_ktime_get_ns();
     data.delta = ts - *tsp;
-    data.ts = ts / 1000;
+    if ((data.delta / 1000000) > ###THRESHOLD###) {
+        data.ts = ts / 1000;
 
-    valp = infobyreq.lookup(&req);
-    if (valp == 0) {
-        data.len = req->__data_len;
-        strcpy(data.name, "?");
-    } else {
-        data.pid = valp->pid;
-        data.len = req->__data_len;
-        bpf_probe_read(&data.name, sizeof(data.name), valp->name);
-        struct gendisk *rq_disk = req->rq_disk;
-        bpf_probe_read(&data.disk_name, sizeof(data.disk_name),
-                       rq_disk->disk_name);
-    }
+        valp = infobyreq.lookup(&req);
+        if (valp == 0) {
+            data.len = req->__data_len;
+            strcpy(data.name, "?");
+        } else {
+            data.pid = valp->pid;
+            data.len = req->__data_len;
+            data.sector = req->__sector;
+            bpf_probe_read(&data.name, sizeof(data.name), valp->name);
+            struct gendisk *rq_disk = req->rq_disk;
+            bpf_probe_read(&data.disk_name, sizeof(data.disk_name),
+                           rq_disk->disk_name);
+        }
 
 /*
  * The following deals with a kernel version change (in mainline 4.7, although
@@ -107,20 +124,36 @@ int trace_req_completion(struct pt_regs *ctx, struct request *req)
  * test, and maintenance burden.
  */
 #ifdef REQ_WRITE
-    data.rwflag = !!(req->cmd_flags & REQ_WRITE);
+        data.rwflag = !!(req->cmd_flags & REQ_WRITE);
 #elif defined(REQ_OP_SHIFT)
-    data.rwflag = !!((req->cmd_flags >> REQ_OP_SHIFT) == REQ_OP_WRITE);
+        data.rwflag = !!((req->cmd_flags >> REQ_OP_SHIFT) == REQ_OP_WRITE);
 #else
-    data.rwflag = !!((req->cmd_flags & REQ_OP_MASK) == REQ_OP_WRITE);
+        data.rwflag = !!((req->cmd_flags & REQ_OP_MASK) == REQ_OP_WRITE);
 #endif
 
-    events.perf_submit(ctx, &data, sizeof(data));
+        events.perf_submit(ctx, &data, sizeof(data));
+    }
     start.delete(&req);
     infobyreq.delete(&req);
 
     return 0;
 }
-""", debug=0)
+"""
+
+# create argument parser
+parser = argparse.ArgumentParser(description='Record disk latency for all IO requests')
+parser.add_argument('--rootdisk', help='root disk to be excluded', default='sdac')
+parser.add_argument('--threshold', help='record disk lantecy exceeding threshold(millisecond)', type=long, default='400')
+parser.add_argument('--period', help='if disk latency exceeds threshold and keeps for a perfiod of time, log error messages', type=long, default='30')
+args = parser.parse_args()
+
+# replace with argument
+bpf_text = bpf_text.replace("##ROOTDISK##", args.rootdisk)
+bpf_text = bpf_text.replace("##ROOTDISKLEN##", str(len(args.rootdisk)))
+bpf_text = bpf_text.replace("###THRESHOLD###", str(args.threshold))
+
+# load BPF program
+b = BPF(text=bpf_text, debug=0)
 b.attach_kprobe(event="blk_account_io_start", fn_name="trace_pid_start")
 b.attach_kprobe(event="blk_start_request", fn_name="trace_req_start")
 b.attach_kprobe(event="blk_mq_start_request", fn_name="trace_req_start")
@@ -135,6 +168,7 @@ class Data(ct.Structure):
         ("pid", ct.c_ulonglong),
         ("rwflag", ct.c_ulonglong),
         ("delta", ct.c_ulonglong),
+        ("sector", ct.c_ulonglong),
         ("len", ct.c_ulonglong),
         ("ts", ct.c_ulonglong),
         ("disk_name", ct.c_char * DISK_NAME_LEN),
@@ -142,13 +176,15 @@ class Data(ct.Structure):
     ]
 
 # header
-print("%-14s %-6s %-7s %-2s %-7s %7s" % ("COMM", "PID",
-    "DISK", "T", "BYTES", "LAT(ms)"))
+print("%-14s %-14s %-6s %-7s %-2s %-9s %-7s %7s" % ("TIME(s)", "COMM", "PID",
+    "DISK", "T", "SECTOR", "BYTES", "LAT(ms)"))
 
 rwflg = ""
 start_ts = 0
 prev_ts = 0
 delta = 0
+high_latency_start = 0
+high_latency_curr= 0
 
 # process event
 def print_event(cpu, data, size):
@@ -158,6 +194,9 @@ def print_event(cpu, data, size):
     global start_ts
     global prev_ts
     global delta
+    global args
+    global high_latency_start
+    global high_latency_curr
 
     if event.rwflag == 1:
         rwflg = "W"
@@ -165,18 +204,33 @@ def print_event(cpu, data, size):
     if event.rwflag == 0:
         rwflg = "R"
 
+    if not re.match(b'\?', event.name):
+        val = event.sector
+
     if start_ts == 0:
         prev_ts = start_ts
 
     if start_ts == 1:
         delta = float(delta) + (event.ts - prev_ts)
 
-    if event.name.decode() != '?':
-        print("%-14.14s %-6s %-7s %-2s %-7s %7.2f" % (
-        event.name.decode(), event.pid,
-        event.disk_name.decode(), rwflg,
+    print("%-14.9f %-14.14s %-6s %-7s %-2s %-9s %-7s %7.2f" % (
+        delta / 1000000, event.name.decode(), event.pid,
+        event.disk_name.decode(), rwflg, val,
         event.len, float(event.delta) / 1000000))
 
+    # track high disk latency start and current timestamp in second
+    # if it keeps happening every second for a 'period' of time, print disk issue
+    # if it doesn't keep happening in 2 seconds, reset counters and start over
+    temp_latency = long(delta / 1000000)
+    if high_latency_start == 0 or temp_latency > high_latency_curr + 1:
+        high_latency_start = temp_latency
+        high_latency_curr = temp_latency
+    else:
+        high_latency_curr = temp_latency
+        if (high_latency_curr - high_latency_start) >= args.period:
+            print("Error: disk issue")
+
+    #print("%d: %d %d" %(args.period, high_latency_start, high_latency_curr))
     prev_ts = event.ts
     start_ts = 1
 
