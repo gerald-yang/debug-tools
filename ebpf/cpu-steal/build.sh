71924d81525b43c24635dda33e21bcff16f10850
Gerald Yang <gerald.yang@canonical.com>
2024-12-13 16:56:26 +0800

    update ebpf

--- ebpf/cpu-steal/build.sh
+++ ebpf/cpu-steal/build.sh
@@ -2,5 +2,5 @@
 
 #bpftool btf dump file /usr/lib/debug/boot/vmlinux-5.4.0-147-generic format c > vmlinux.h
 GOPACKAGE=main bpf2go -cc clang-10 -cflags '-O2 -g -Wall -Werror' -target bpfel bpf tracepoint.c -- -I /home/ubuntu/go/pkg/mod/github.com/cilium/ebpf@v0.11.0/examples/headers
-go build -o cpu-steal main.go bpf_bpfel.go
+go build -o vm-exit main.go bpf_bpfel.go
 
