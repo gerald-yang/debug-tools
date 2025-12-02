e4cc9138464f2d4ad60ba4f337c4ac0807ae7e86
Gerald Yang <gerald.yang@canonical.com>
2025-12-02 22:54:16 +0800

    simplify script

--- ext4/trigger-call-trace.sh
+++ ext4/trigger-call-trace.sh
@@ -5,35 +5,34 @@ if ! [ $(id -u) -eq 0 ]; then
         exit -1
 fi
 
-DISK="$1"
-FOLDER="$2"
-
-if [ -z "$DISK" ] || [ -z "$FOLDER" ]; then
-        echo "Usage:"
-        echo "./trigger-ext4-call-trace.sh <ext4 disk> <mount folder>"
-        exit -1
+if ! dpkg -s fio >/dev/null 2>&1; then
+	        echo "install fio"
+		        apt install fio -y
 fi
+
+DISK="testdisk"
+FOLDER="mnt"
+
+mkdir -p "$FOLDER"
 
 if mountpoint -q "$FOLDER"; then
-        echo "umount $FOLDER"
-        umount "$FOLDER"
+	        echo "umount $FOLDER"
+		        umount "$FOLDER"
 fi
 
-journal_data=$(tune2fs -l "$DISK" | grep journal_data)
-if [ -z "$journal_data" ]; then
-        echo "journal_data not set, set it"
-        tune2fs -o journal_data "$DISK"
-fi
-
-if ! dpkg -s fio >/dev/null 2>&1; then
-	echo "install fio"
-	apt install fio -y
-fi
+rm -f "$DISK"
+dd if=/dev/random of="$DISK" bs=1G count=2 oflag=direct
+mkfs.ext4 "$DISK"
+tune2fs -o journal_data "$DISK"
 
 mount "$DISK" "$FOLDER"
 fio --name=fiotest --rw=randwrite --bs=4k --runtime=3 --ioengine=libaio --iodepth=128 --numjobs=4 --filename="$FOLDER"/fiotest --filesize=1G --group_reporting
 mount -o remount,ro "$DISK" "$FOLDER"
 sync
 
+umount "$FOLDER"
+rm -f "$DISK"
+
 echo ""
 echo "check dmesg"
+dmesg | tail -n 20
