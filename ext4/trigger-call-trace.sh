dbc342d8fd5f09883d320cdbea0acf61672c25e7
Gerald Yang <gerald.yang@canonical.com>
2025-12-02 15:39:11 +0800

    install fio

--- ext4/trigger-call-trace.sh
+++ ext4/trigger-call-trace.sh
@@ -25,9 +25,15 @@ if [ -z "$journal_data" ]; then
         tune2fs -o journal_data "$DISK"
 fi
 
+if ! dpkg -s fio >/dev/null 2>&1; then
+	echo "install fio"
+	apt install fio -y
+fi
+
 mount "$DISK" "$FOLDER"
 fio --name=fiotest --rw=randwrite --bs=4k --runtime=3 --ioengine=libaio --iodepth=128 --numjobs=4 --filename="$FOLDER"/fiotest --filesize=1G --group_reporting
 mount -o remount,ro "$DISK" "$FOLDER"
 sync
 
+echo ""
 echo "check dmesg"
